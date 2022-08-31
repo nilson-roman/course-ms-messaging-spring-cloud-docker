3a2eef9e885152bcd9ea1c7c9ac5e6d785e6fc9b
Nilson Roman <nilsonoroman@gmail.com>
2022-08-30 21:15:40 -0300

    Completing and testing Clientes microservice

--- msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClienteController.java
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClienteController.java
@@ -1,15 +1,43 @@
 package io.github.cursodsousa.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.cursodsousa.msclientes.application.representation.ClienteSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status(){
         return "Ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request){
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = service.getByCpf(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
