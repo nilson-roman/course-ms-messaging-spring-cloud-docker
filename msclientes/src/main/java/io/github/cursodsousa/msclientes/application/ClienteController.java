6f7188cee9b62b803324ecc212fdec061cbcc956
Nilson Roman <nilsonoroman@gmail.com>
2022-08-30 22:42:50 -0300

    Testing Load Balancer with Gateway

--- msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClienteController.java
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.cursodsousa.msclientes.application;
 
 import io.github.cursodsousa.msclientes.application.representation.ClienteSaveRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,12 +12,14 @@ import java.net.URI;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClienteController {
 
     private final ClienteService service;
 
     @GetMapping
     public String status(){
+        log.info("Obtendo o status do microservice de Clientes");
         return "Ok";
     }
 
